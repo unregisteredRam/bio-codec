cc443ed1032b4c1514286604a5c566bd169680a5
unregisteredRam <aldreesm2007@gmail.com>
2025-06-22 00:51:56 +0300

    Add files via upload

---
+++ basic mathematical function.py
@@ -0,0 +1,15 @@
+#basic mathematical function
+a=input('Enter the first number :')
+a=float(a)
+b=input('Enter the second number :')
+b=float(b)
+
+c=a+b
+print('addition =',c)
+d=a-b
+print('subtraction =',d)
+e=a*b
+print('multiplication =',e)
+f=a/b
+print('division =',f)
+
